ca51faa9527496609800edd48f4e0a1b067f03af
Mendenhk <kris_mendenhall@yahoo.com>
2026-01-13 22:49:06 -0800

    added css and date and place of birth javaScript

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -17,14 +17,20 @@ const displayProphets = (prophets) => {
     prophets.forEach((prophet) => {
         let card = document.createElement('section');
         let fullName = document.createElement('h2');
+        let birthdate = document.createElement('p');
+        let birthplace = document.createElement('p');
         let portrait = document.createElement('img');
         fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        birthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
         portrait.setAttribute('src', prophet.imageurl);
         portrait.setAttribute('alt', `Protrait of ${prophet.name} ${prophet.lastname}`);
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '500');
         portrait.setAttribute('length', '300');
         card.appendChild(fullName);
+        card.appendChild(birthdate);
+        card.appendChild(birthplace);
         card.appendChild(portrait);
         cards.appendChild(card);
     });
