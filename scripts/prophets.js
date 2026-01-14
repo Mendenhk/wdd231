d7cd530aad728f4e8fa6a75a350924d9fb3fc300
Mendenhk <kris_mendenhall@yahoo.com>
2026-01-13 16:39:59 -0800

    created displayProphets function

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -7,6 +7,25 @@ async function getProphetData() {
     //above:  data request from website.  await makes further code pause until data returned.
     const data = await response.json();
     //above: parses JSON to object and saves in the variable "data".  await used to pause code until finished.
-    console.log(data); // temp output test of data response 
+    // console.table(data.prophets);
+    // above: temp output test of data response.  data.prophets because data is an object containing a single array of objects called prophets.  Table works best with arrays directly, not objects.
+    displayProphets(data.prophets);
 }
 getProphetData();  //call to the above function 
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        portrait.setAttribute('src', `Protrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('alt', fullName);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '500');
+        portrait.setAttribute('length', '300');
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+        cards.appendChild(card);
+    });
+}
