5c2960c1ad0dbb32397d31b00383c7edee4845e9
kris mendenhall <kris_mendenhall@yahoo.com>
2026-01-15 21:17:34 -0800

    remove duplicate chamber files

--- chamber 2/scripts/navigation.js
+++
@@ -1,9 +0,0 @@
-const navButton = document.querySelector('#ham-btn');
-const navLinks = document.querySelector('#nav-bar');
-
-navButton.addEventListener('click', () => {
-    navButton.classList.toggle('show');
-    navLinks.classList.toggle('show');
-});
-
-
