9ccddde8942306663d5d2ce884abb8d953d7d2ad
Mendenhk <kris_mendenhall@yahoo.com>
2026-01-16 18:12:25 -0800

    linked cards.js to html, and extracted data from the business data file.  tested output with table

--- chamber/scripts/cards.js
+++ chamber/scripts/cards.js
@@ -1,17 +1,20 @@
-const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
-//above: JSON array of objects containing prophet information
+// Last checked here
+
+
+const businessData = 'data/members.json';
+//above: JSON array of objects containing business information
 const cards = document.getElementById('cards');
 
-async function getProphetData() {
-    const response = await fetch(url);
-    //above:  data request from website.  await makes further code pause until data returned.
+async function getBusinessData() {
+    const response = await fetch(businessData);
+    //above:  data request from JSON formated file.  await makes further code pause until data returned.
     const data = await response.json();
     //above: parses JSON to object and saves in the variable "data".  await used to pause code until finished.
-    // console.table(data.prophets);
-    // above: temp output test of data response.  data.prophets because data is an object containing a single array of objects called prophets.  Table works best with arrays directly, not objects.
-    displayProphets(data.prophets);
+    console.table(data.companies);
+    // above: temp output test of data response.  data.companies because data is an object containing a single array of objects called prophets.  Table works best with arrays directly, not objects.
+    // displayProphets(data.prophets);
 }
-getProphetData();  //call to the above function 
+getBusinessData();  //call to the above function 
 
 const displayProphets = (prophets) => {
     prophets.forEach((prophet) => {
