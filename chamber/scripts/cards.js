e53c7a6df73babd558b547ca3a0472db4796bb9e
kris mendenhall <kris_mendenhall@yahoo.com>
2026-01-22 16:39:40 -0800

    dont remember

--- chamber/scripts/cards.js
+++ chamber/scripts/cards.js
@@ -1,6 +1,4 @@
-// Last checked here
-
-
+// ---------------- FETCHING AND DISPLAYING BUSINESS CARD DATA ----------------
 const businessData = 'data/members.json';
 //above: JSON array of objects containing business information
 const cards = document.getElementById('cards');
@@ -91,10 +89,59 @@ const displayBusinesses = (businesses) => {
   }
 }
 
-// ---------------- BUTTON FUNCTION ----------------
+// ---------------- LIST OR CARDS BUTTON FUNCTION ----------------
 const listCardButton = document.querySelector('#listOrCards');
 listCardButton.addEventListener('click', () => {
   cards.classList.toggle('list');
   listCardButton.classList.toggle('list');
   displayBusinesses(businesses);
 });
+
+// ---------------- SPOTLIGHT BUSINESS CARDS ----------------
+// Check all of the following code for correctness
+const spotlightCards = document.getElementById('spotlightCards');
+
+function displaySpotlightMembers(members) {
+  // const spotlightMembers = members.filter(member => member.membershipLevel === 'Gold' || member.membershipLevel === 'Silver');
+  // const selectedMembers = [];
+  // while (selectedMembers.length < 3 && spotlightMembers.length > 0) {
+  //   const randomIndex = Math.floor(Math.random() * spotlightMembers.length);
+  //   selectedMembers.push(spotlightMembers.splice(randomIndex, 1)[0]);
+  // }
+  // selectedMembers.forEach((member) => {
+  //   let card = document.createElement('section');
+  //   let name = document.createElement('h3');
+  //   let tagline = document.createElement('p');
+  //   let image = document.createElement('img');
+  //   let contact = document.createElement('div');
+  //   let email = document.createElement('p');
+  //   let phone = document.createElement('p');
+  //   let url = document.createElement('p');
+  //   card.setAttribute('class', 'card');
+  //   name.innerHTML = `${member.name}`;
+  //   tagline.textContent = `${member.tagline}`;
+  //   tagline.setAttribute('class', 'tagline')
+  //   email.textContent = `EMAIL: ${member.email}`;
+  //   phone.textContent = `PHONE: ${member.phone}`;
+  //   url.textContent = `URL: ${member.URL}`;
+  //   image.setAttribute('src', member.image);
+  //   image.setAttribute('alt', `${member.name} photograph`);
+  //   image.setAttribute('loading', 'lazy');
+  //   image.setAttribute('width', '100');
+  //   image.setAttribute('length', '100');
+  //   contact.setAttribute('id', 'contact')
+  //   contact.appendChild(email);
+  //   contact.appendChild(phone);
+  //   contact.appendChild(url);
+  //   card.appendChild(name);
+  //   card.appendChild(tagline);
+  //   card.appendChild(image);
+  //   card.appendChild(contact);
+
+    spotlightCards.appendChild(card);
+  });
+}
+
+// getBusinessData().then(() => {
+//   displaySpotlightMembers(businesses);
+// });
