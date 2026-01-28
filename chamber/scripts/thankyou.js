1a729e0a6bafeae243de4fb45901b80672e84f2b
Mendenhk <kris_mendenhall@yahoo.com>
2026-01-27 18:10:09 -0800

    copied and pasted thank you page an js from code along homework

---
+++ chamber/scripts/thankyou.js
@@ -0,0 +1,13 @@
+// below is just an example of thank you js from our homework
+
+// const getString = window.location.search;
+// console.log(getString);
+
+// const myInfo = new URLSearchParams(getString);
+// console.log(myInfo);
+
+// document.querySelector('#results').innerHTML = `
+//   <p>Appointment for ${myInfo.get('first')} ${myInfo.get('last')} </p> 
+//   <p>Proxy ${myInfo.get('ordinance')} on ${myInfo.get('date')} in the ${myInfo.get('location')} Temple</p>
+//   <p>Your Phone: ${myInfo.get('phone')}</p>
+//   <p>Your email is: ${myInfo.get('email')}</p>`;
