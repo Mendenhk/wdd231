101260b0861794fac2716e6325512d125590978f
Mendenhk <kris_mendenhall@yahoo.com>
2026-01-27 23:44:16 -0800

    added javascript for extracting benefits from membership data and putting in a bulleted list.

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -11,3 +11,11 @@ document.querySelector('#results').innerHTML = `
   <p>Proxy ${myInfo.get('ordinance')} on ${myInfo.get('date')} in the ${myInfo.get('location')} Temple</p>
   <p>Your Phone: ${myInfo.get('phone')}</p>
   <p>Your email is: ${myInfo.get('email')}</p>`;
+
+
+//the following is code for extracting the benefits key from the membership database and placing them in a bulleted list:
+// membership.benefits.forEach(benefit => {
+//   const li = document.createElement("li");
+//   li.textContent = benefit;
+//   ul.appendChild(li);
+// });
