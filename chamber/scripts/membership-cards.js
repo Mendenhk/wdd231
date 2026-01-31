481eda29f9ddcee191f344513c8a27657d8c10c8
Mendenhk <kris_mendenhall@yahoo.com>
2026-01-30 21:35:52 -0800

    unsuccessfully tried to fix close button for modal

--- chamber/scripts/membership-cards.js
+++ chamber/scripts/membership-cards.js
@@ -71,13 +71,13 @@ const myTitle = document.querySelector('#dialog-box H2');
 const myCost = document.querySelector('#my-cost');
 const myBenefits = document.querySelector('#my-benefits');
 const myBenefitsList = document.querySelector('#dialog-box ul');
-// const closeButton = document.querySelector('#dialog-box button');
-// closeButton.addEventListener('click', () => dialogBox.close());
+const closeButton = document.querySelector('#dialog-box button');
+closeButton.addEventListener('click', () => dialogBox.close());
 
 function showStuff(x) {
-  const closeButton = document.createElement('button');
-  closeButton.innerHTML = "Close";
-  closeButton.addEventListener('click', () => dialogBox.close());
+  // const closeButton = document.createElement('button');
+  // closeButton.innerHTML = "Close";
+  // closeButton.addEventListener('click', () => dialogBox.close());
   myTitle.innerHTML = x.title;
   myCost.innerHTML = `Yearly Cost: $${x.cost}`;
   myBenefits.innerHTML = 'Benefits:';
