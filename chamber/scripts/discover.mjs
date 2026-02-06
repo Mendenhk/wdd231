c97cec120b533d07a728ef43519b88c7c90fd950
Mendenhk <kris_mendenhall@yahoo.com>
2026-02-05 22:50:43 -0800

    added button and button css to small and medium screen (button hovers ofer image

--- chamber/scripts/discover.mjs
+++ chamber/scripts/discover.mjs
@@ -8,17 +8,19 @@ function displayDiscoverCards(places) {
   places.forEach((level) => {
     let dCard = document.createElement('section');
     let myName = document.createElement('h2');
+    let myButton = document.createElement('button');
     let figure = document.createElement('figure');
     let myPhoto = document.createElement('img');
     let myAddress = document.createElement('address');
-    let myCity = document.createElement('p');
     let myDescription = document.createElement('p');
     dCard.setAttribute('class', 'dCard');
     myName.innerHTML = `${level.name}`;
+    myButton.textContent = 'Learn More';
     myPhoto.setAttribute('src', level.image);
     myPhoto.setAttribute('alt', level.name);
     myPhoto.setAttribute('loading', 'lazy');
     figure.appendChild(myPhoto);
+    figure.appendChild(myButton);
     myAddress.innerHTML = `${level.address}<br>${level.city}`;
     myDescription.innerHTML = `${level.description}`;
     dCard.appendChild(myName);
