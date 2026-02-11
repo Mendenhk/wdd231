cbacacf5dcd6a63f74585dcc9c565340c498a6be
Mendenhk <kris_mendenhall@yahoo.com>
2026-02-11 02:45:13 -0800

    created nail category cards with javaScript and added event listener to modal buttons.  started modal function and modal CSS

--- finalproject/scripts/index.js
+++ finalproject/scripts/index.js
@@ -8,41 +8,47 @@ displayCategories(categories);
 // ----------- DISPLAYING MEMBERSHIP LEVEL CARDS -----------
 
 function displayCategories(nailTypes) {
-  const levelsSection = document.querySelector('#nails-section');
+  const nailsSection = document.querySelector('#nails-section');
   nailTypes.forEach((nailType) => {
-  //   let card = document.createElement('section');
-  //   let title = document.createElement('h3');
-  //   let benefitButton = document.createElement('button');
-  //   card.setAttribute('class', 'mCard');
-  //   title.innerHTML = `${level.title}`;
-  //   benefitButton.innerHTML = 'Learn More';
-  //   benefitButton.addEventListener('click', () => showStuff(level));
-  //   card.appendChild(title);
-  //   card.appendChild(benefitButton);
-  //   levelsSection.appendChild(card);
+    let myCard = document.createElement('div');
+    let myImage = document.createElement('img');
+    let myDiv = document.createElement('div');
+    let myTitle = document.createElement('h3');
+    let modalButton = document.createElement('button');
+    myDiv.setAttribute('class', 'button-div');
+    myImage.setAttribute('src', `${nailType.image}`);
+    myImage.setAttribute('alt', `${nailType.imageAlt}`)
+    myTitle.innerHTML = `${nailType.title}`;
+    modalButton.innerHTML = 'Learn More >>';
+    modalButton.addEventListener('click', () => showStuff(nailTypes));
+    myDiv.appendChild(myTitle);
+    myDiv.appendChild(modalButton);
+    myCard.appendChild(myImage);
+    myCard.appendChild(myDiv);
+    nailsSection.appendChild(myCard);
   });
 
 }
 
 // ---------------- MODAL CONSTANTS AND FUNCTIONS ----------------
 const dialogBox = document.querySelector('#dialog-box');
-const myTitle = document.querySelector('#dialog-box H2');
-const myCost = document.querySelector('#my-cost');
-const myBenefits = document.querySelector('#my-benefits');
-const myBenefitsList = document.querySelector('#dialog-box ul');
-const closeButton = document.querySelector('#dialog-box button');
-closeButton.addEventListener('click', () => dialogBox.close());
+const dialogTitle = document.querySelector('#dialog-box H3');
+// const myCost = document.querySelector('#my-cost');
+// const myBenefits = document.querySelector('#my-benefits');
+// const myBenefitsList = document.querySelector('#dialog-box ul');
+// const closeButton = document.querySelector('#dialog-box button');
+// closeButton.addEventListener('click', () => dialogBox.close());
 
 function showStuff(x) {
-  dialogBox.setAttribute('class', x.memberLevel);
-  myTitle.innerHTML = x.title;
-  myCost.innerHTML = `Yearly Cost: $${x.cost}`;
-  myBenefits.innerHTML = 'Benefits:';
-  let benefitList = ``;
-  x.benefits.forEach((line) => {
-    const listLine = `<li>${line}</li>`;
-    benefitList += listLine;
-  });
-  myBenefitsList.innerHTML = benefitList;
+  // dialogBox.setAttribute('class', x.memberLevel);
+  // myTitle.innerHTML = x.title;
+  // myCost.innerHTML = `Yearly Cost: $${x.cost}`;
+  // myBenefits.innerHTML = 'Benefits:';
+  // let benefitList = ``;
+  // x.benefits.forEach((line) => {
+  //   const listLine = `<li>${line}</li>`;
+  //   benefitList += listLine;
+  // });
+  // myBenefitsList.innerHTML = benefitList;
   dialogBox.showModal();
 }
