435af0222bc03c7630c4b7c65879387f7a025783
kris mendenhall <kris_mendenhall@yahoo.com>
2026-02-12 22:50:22 -0800

    finished other two sections and the cards

--- finalproject/scripts/designs.js
+++ finalproject/scripts/designs.js
@@ -69,77 +69,44 @@ function displayNails(stylesObject) {
 		elegantSection.appendChild(card);
 		cards.appendChild(elegantSection);
 		});
-//     nails.forEach((style) => {
-    //   let card = document.createElement('section');
-    //   let name = document.createElement('h2');
-    //   // let tagline = document.createElement('p');
-    //   // let image = document.createElement('img');
-    //   let contact = document.createElement('div');
-    //   let email = document.createElement('p');
-    //   let phone = document.createElement('p');
-    //   let url = document.createElement('a');
-    //   card.setAttribute('class', 'card');
-    //   name.innerHTML = `${style.name}`;
-    //   // tagline.textContent = `${style.tagline}`;
-    //   // tagline.setAttribute('class', 'tagline')
-    //   email.textContent = `EMAIL: ${style.email}`;
-    //   phone.textContent = `PHONE: ${style.phone}`;
-    //   url.textContent = `URL: ${style.URLName}`;
-    //   url.setAttribute('href', style.URL);
-    //   url.setAttribute('target', '_blank');
-    //   url.setAttribute('class', 'list');
-    //   // image.setAttribute('src', style.image);
-    //   // image.setAttribute('alt', `${style.name} photograph`);
-    //   // image.setAttribute('loading', 'lazy');
-    //   // image.setAttribute('width', '100');
-    //   // image.setAttribute('length', '100');
-    //   contact.setAttribute('id', 'contact')
-    //   contact.appendChild(email);
-    //   contact.appendChild(phone);
-    //   contact.appendChild(url);
-    //   card.appendChild(name);
-    //   // card.appendChild(tagline);
-    //   // card.appendChild(image);
-    //   card.appendChild(contact);
 
-      cards.appendChild(threeDSection);
-      cards.appendChild(playfulSection);
-//     });
-//   }
-//   else {
-//     nails.forEach((style) => {
-//       let card = document.createElement('section');
-    //   let name = document.createElement('h2');
-    //   let tagline = document.createElement('p');
-    //   let image = document.createElement('img');
-    //   let contact = document.createElement('div');
-    //   let email = document.createElement('p');
-    //   let phone = document.createElement('p');
-    //   let url = document.createElement('a');
-    //   card.setAttribute('class', 'card');
-    //   name.innerHTML = `${style.name}`;
-    //   tagline.textContent = `${style.tagline}`;
-    //   tagline.setAttribute('class', 'tagline')
-    //   email.textContent = `EMAIL: ${style.email}`;
-    //   phone.textContent = `PHONE: ${style.phone}`;
-    //   url.textContent = `URL: ${style.URLName}`;
-    //   image.setAttribute('src', style.image);
-    //   image.setAttribute('alt', `${style.name} photograph`);
-    //   image.setAttribute('loading', 'lazy');
-    //   image.setAttribute('width', '100');
-    //   image.setAttribute('height', '100');
-    //   url.setAttribute('href', style.URL);
-    //   url.setAttribute('target', '_blank');
-    //   contact.setAttribute('id', 'contact');
-    //   contact.appendChild(email);
-    //   contact.appendChild(phone);
-    //   contact.appendChild(url);
-    //   card.appendChild(name);
-    //   card.appendChild(tagline);
-    //   card.appendChild(image);
-    //   card.appendChild(contact);
+		threeDList.forEach((item) => {
+			let card = document.createElement('div');
+			let myImage = document.createElement('img');
+			let myName = document.createElement('h3');
+			let singleCost = document.createElement('p');
+			let setCost = document.createElement('p');
+			
+			myImage.setAttribute('src', `${item.image}`);
+			myImage.setAttribute('alt', `${item.name}`);
+			myName.innerHTML = `${item.name}`;
+			singleCost.innerHTML = `Price per nail: $${item.singlePrice}`;
+			setCost.innerHTML = `Price per set (10): $${item.setPrice}`;
+			card.appendChild(myImage);
+			card.appendChild(myName);
+			card.appendChild(singleCost);
+			card.appendChild(setCost);
+			threeDSection.appendChild(card);
+			cards.appendChild(threeDSection);
+		});
 
-    //   cards.appendChild(card);
-    // });
-//   }
+		playfulList.forEach((item) => {
+		let card = document.createElement('div');
+		let myImage = document.createElement('img');
+		let myName = document.createElement('h3');
+		let singleCost = document.createElement('p');
+		let setCost = document.createElement('p');
+		
+		myImage.setAttribute('src', `${item.image}`);
+		myImage.setAttribute('alt', `${item.name}`);
+		myName.innerHTML = `${item.name}`;
+		singleCost.innerHTML = `Price per nail: $${item.singlePrice}`;
+		setCost.innerHTML = `Price per set (10): $${item.setPrice}`;
+		card.appendChild(myImage);
+		card.appendChild(myName);
+		card.appendChild(singleCost);
+		card.appendChild(setCost);
+		playfulSection.appendChild(card);
+		cards.appendChild(playfulSection);
+		});
 }
